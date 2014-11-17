e5570ae57cfe8f91ba4cf90c833818fb55bbdffc
Ben Drucker <bvdrucker@gmail.com>
2014-11-17 08:50:06 -0800

    Add a functional node and browserify example

---
+++ examples/node-browserify/test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var expect = require('chai').expect;
+var sinon  = require('sinon');
+
+require('sinon-as-promised');
+
+
+describe('sinon-as-promised node example', function () {
+
+  it('can create a stub that resolves', function () {
+    var stub = sinon.stub().resolves('value');
+    return stub().then(function (value) {
+      expect(value).to.equal('value');
+    });
+  });
+
+});
