02ef10405ee47c6f4e42c5e1fb8d4aeef5b273a2
sainthkh <sainthkh@naver.com>
2016-12-08 21:09:56 +0900

    Added promised spy.

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ function rejects (err) {
 sinon.stub.rejects = rejects
 sinon.behavior.rejects = rejects
 
+function spyResolves(val) {
+  var spy = this
+  spy.promised = function() {
+    spy.apply(this, arguments)
+    return createThenable(Promise, function (resolve) {
+      resolve(val)
+    })
+  }
+  return spy
+}
+
+sinon.spy.resolves = spyResolves
+
+function spyRejects(err) {
+  var spy = this
+  spy.promised = function() {
+    spy.apply(this, arguments)
+    return createThenable(Promise, function (resolve, reject) {
+      reject(err)
+    })
+  }
+  return spy
+}
+
+sinon.spy.rejects = spyRejects
+
 module.exports = function (_Promise_) {
   if (typeof _Promise_ !== 'function') {
     throw new Error('A Promise constructor must be provided')
